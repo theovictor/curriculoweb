f458d8f0928936bfa396a8f3d69d92d4bc6b815b
Theo Victor <teo.pvh@gmail.com>
2021-03-29 13:23:25 -0400

    adicionado biblioteca argon

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -15,3 +15,26 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/css/argon-design-system-react.css";
+
+import Profile from "views/Profile.js";
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact render={props => <App {...props} />} />
+            <Route path="/profile-page" exact render = {props => <Profile {...props}/>}/>
+            <Redirect to="/"/>
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
