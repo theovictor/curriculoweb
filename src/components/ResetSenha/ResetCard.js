361434249ca5a1e76ebf437404bb18ed652aab8d
Theo Victor <teo.pvh@gmail.com>
2021-05-11 20:24:41 -0400

    implementado formik input

--- src/components/ResetSenha/ResetCard.js
+++ src/components/ResetSenha/ResetCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "assets/css/ResetPage.css";
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import {
   Button,
   Card,
@@ -14,55 +15,60 @@ import {
   InputGroup,
   InputGroupAddon,
   InputGroupText,
+  FormFeedback
 } from "reactstrap";
-function ResetCard() {
-    const [emailFocus, setEmailFocus] = React.useState("");
-    return(
-        <>
-            <Card className="bg-secondary shadow border-0">
-                <CardHeader>
-                    <CardImg alt="..."
-                        src={require("assets/img/logo.png")}
-                    ></CardImg>
-                    <CardTitle className="text-center" tag="h4">
-                        Resetar Senha
+export default function ResetCard() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: yup.object({
+      email: yup.string().email('Email inválido').required('Insira seu Email!'),
+    })
+  })
+  return (
+    <>
+      <Card className="bg-secondary shadow border-0">
+        <CardHeader>
+          <CardImg alt="..."
+            src={require("assets/img/logo.png")}
+          ></CardImg>
+          <CardTitle className="text-center" tag="h4">
+            Resetar Senha
                     </CardTitle>
-                </CardHeader>
-                <CardBody className="px-lg-5 py-lg-5">
-                    <div className="text-center text-muted mb-4">
-                        <smal>Digite o seu e-mail para redefinir a senha</smal>
-                    </div>
-                    <Form role="form">
-                        <FormGroup className={"mb-3" + emailFocus}>
-                            <InputGroup className="input-group-alternative">
-                                <InputGroupAddon addonType="prepend">
-                                    <InputGroupText>
-                                        <i className="ni ni-email-83"/>
-                                    </InputGroupText>
-                                </InputGroupAddon>
-                                <Input
-                                    placeholder="Email"
-                                    type="email"
-                                    onFocus={() => setEmailFocus("focused")}
-                                    onBlur={() => setEmailFocus("")}
-                                />
-                            </InputGroup>
-                        </FormGroup>
-                        <div className="text-center">
-                            <Button className="my-4" color="primary" type="button">
-                                Enviar
-                            </Button>
-                        </div>
-                        <div className="text-center">
-                            <i className="fa fa-hand-point-left mr-1"/>
-                            <Link to="/index-page" tag={Link}>
-                                Voltar
-                            </Link>
-                        </div>
-                    </Form>
-                </CardBody>
-            </Card>
-        </>
-    );
+        </CardHeader>
+        <CardBody className="px-lg-5 py-lg-5">
+          <div className="text-center text-muted mb-4">
+            <smal>Digite o seu e-mail para redefinir a senha</smal>
+          </div>
+          <Form role="form" onSubmit={formik.handleSubmit}>
+            <FormGroup className="mb-3">
+              <InputGroup className="input-group-alternative">
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>
+                    <i className="ni ni-email-83" />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input placeholder="Email" type="email" id="email"
+                  invalid={formik.touched.email && formik.errors.email ? true : false}
+                  {...formik.getFieldProps('email')} />
+                <FormFeedback>{formik.touched.email && formik.errors.email ? formik.errors.email : null}</FormFeedback>
+              </InputGroup>
+            </FormGroup>
+            <div className="text-center">
+              <Button className="my-4" color="primary" type="button">
+                Enviar
+              </Button>
+            </div>
+            <div className="text-center">
+              <i className="fa fa-hand-point-left mr-1" />
+              <Link to="/index-page" tag={Link}>
+                Voltar
+              </Link>
+            </div>
+          </Form>
+        </CardBody>
+      </Card>
+    </>
+  );
 }
-export default ResetCard;
--- src/pages/ResetPage.js
+++ src/pages/ResetPage.js
@@ -1,37 +1,37 @@
 import React from "react";
 import { Container, Col } from "reactstrap";
+import "assets/css/ResetPage.css";
 import ResetCard from "components/ResetSenha/ResetCard.js";
-function ResetPage() {
-    React.useEffect(() => {
-        document.body.classList.add("reset-page");
-        window.scrollTo(0, 0);
-        document.body.scrollTop = 0;
-        return function cleanup() {
-            document.body.classList.remove("reset-page");
-        };
-    });
-    return (
-        <>
-            <div className="wrapper">
-                <section className="section section-shaped section-lg">
-                    <div className="shape shape-style-1 bg-gradient-teal">
-                        <span/>
-                        <span/>
-                        <span/>
-                        <span/>
-                        <span/>
-                        <span/>
-                        <span/>
-                        <span/>
-                    </div>
-                    <Container>
-                        <Col className="mx-auto" lg="5" mb="1">
-                            <ResetCard />
-                        </Col>
-                    </Container>
-                </section>
-            </div>
-        </>
-    );
+export default function ResetPage() {
+  React.useEffect(() => {
+    document.body.classList.add("reset-page");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("reset-page");
+    };
+  });
+  return (
+    <>
+      <div className="wrapper">
+        <section className="section section-shaped section-lg">
+          <div className="shape shape-style-1 bg-gradient-teal">
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+          <Container>
+            <Col className="mx-auto" lg="5" mb="1">
+              <ResetCard />
+            </Col>
+          </Container>
+        </section>
+      </div>
+    </>
+  );
 }
-export default ResetPage;
