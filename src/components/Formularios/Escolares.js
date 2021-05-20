98651645c3de2367919246abbc2bf24a9bde83be
Theo Victor <teo.pvh@gmail.com>
2021-05-20 00:30:13 -0400

    Modal para escolares

--- src/components/Formularios/Escolares.js
+++ src/components/Formularios/Escolares.js
@@ -2,41 +2,15 @@ import React from "react";
 // react component for creating dynamic tables
 import BootstrapTable from "react-bootstrap-table-next";
 import ToolkitProvider from "react-bootstrap-table2-toolkit";
-import { Button, ButtonGroup, Card, Container, Row, Col, UncontrolledTooltip} from 'reactstrap';
+import { Card, Container, Row, Col } from 'reactstrap';
 import { dataTable } from 'components/variables/general.js';
-
-// const { SearchBar } = Search;
+import ModalEscola from 'components/Modal/ModalEscola.js';
 
 export default function Escolares(){
-  const componentRef = React.useRef(null);
-  const copyToClipboardAsTable = (el) => {
-    var body = document.body,
-      range,
-      sel;
-    if (document.createRange && window.getSelection) {
-      range = document.createRange();
-      sel = window.getSelection();
-      sel.removeAllRanges();
-      try {
-        range.selectNodeContents(el);
-        sel.addRange(range);
-      } catch (e) {
-        range.selectNode(el);
-        sel.addRange(range);
-      }
-      document.execCommand("copy");
-    } else if (body.createTextRange) {
-      range = body.createTextRange();
-      range.moveToElementText(el);
-      range.select();
-      range.execCommand("Copy");
-    }
-  };
   return (
     <>
       <Container fluid>
         <Row>
-          <div className="col">
             <Card>
               <ToolkitProvider
                   data={dataTable}
@@ -88,33 +62,20 @@ export default function Escolares(){
                     <div className="py-4 table-responsive">
                       <Container fluid>
                         <Row>
-                          <Col xs={12} sm={6}>
-                            <ButtonGroup>
-                              <Button
-                                className="buttons-copy buttons-html5"
-                                color="default"
-                                size="sm"
-                                id="copy-tooltip"
-                                onClick={() => {}}
-                              >
-                                <span>Copy</span>
-                              </Button>
-                            </ButtonGroup>
+                          <Col md={4} sm={6}>
+                            <ModalEscola/>
                           </Col>
                         </Row>
                       </Container>
                       <BootstrapTable
-                        ref={componentRef}
                         {...props.baseProps}
                         bootstrap4={true}
                         bordered={false}
-                        id="react-bs-table"
                       />
                     </div>
                   )}
                 </ToolkitProvider>
             </Card>
-          </div>
         </Row>
       </Container>
     </>
---
+++ src/components/Modal/ModalEscola.js
@@ -0,0 +1,145 @@
+import React, {useState} from 'react';
+import { Button, Modal, Input, Form, Row, Col, FormGroup, FormFeedback, Label, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import ReactDatetime from 'react-datetime';
+import 'moment/locale/pt-br';
+
+export default function ModalEscola() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      escola: '',
+      curso: '',
+      inicio: '',
+      termino: '',
+      periodo: '',
+      turno: '',
+      status: '',
+    },
+    validationSchema: yup.object({
+      escola: yup.string().required('O campo Escola/Universidade é obrigatório.'),
+      curso: yup.string().required('O campo Curso é obrigatório.'),
+      inicio: yup.string().required('O campo Início é obrigatório.'),
+      termino: yup.string().required('O campo Término é obrigatório.'),
+      periodo: yup.string().required('O campo Período é obrigatório.'),
+      turno: yup.string().required('O campo Turno é obrigatório.'),
+      status: yup.string().required('O campo Status é obrigatório.'),
+    }),
+  });
+  return (
+    <>
+      <Button className="mb-3" color="primary" type="button" onClick={() => setModalOpen(!modalOpen)}>
+        <span className="btn-inner--icon">
+          <i className="ni ni-fat-add"></i>
+        </span>
+        <span className="btn-inner--text">Novo</span>
+      </Button>
+      <Modal className="modal-lg" isOpen={modalOpen} toggle={() => setModalOpen(!modalOpen)} modalClassName="bd-example-modal-lg">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalScrollableTitle">
+            Adicionar Curso
+          </h5>
+          <button aria-label="Close" className="close" type="button" onClick={() => setModalOpen(!modalOpen)}>
+            <span aria-hidden={true}>
+              <i className="ni ni-fat-remove"/>
+            </span>
+          </button>
+        </div>
+        <div className="modal-body bg-secondary">
+          <Form onSubmit={formik.handleSubmit}>
+            <Row>
+              <FormGroup>
+                <Label className="form-control-label" htmlFor="escola">Escola/Universidade *</Label>
+                <Input className="form-control-alternative" id="escola" placeholder="Nome da Escola/Universidade" type="text"
+                  invalid={formik.touched.escola && formik.errors.escola ? true : false}
+                  {...formik.getFieldProps('escola')}/>
+                <FormFeedback>{formik.touched.escola && formik.errors.escola ? formik.errors.escola : null}</FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Label className="form-control-label" htmlFor="curso">Curso *</Label>
+                <Input className="form-control-alternative" id="curso" placeholder="Curso" type="text"
+                  invalid={formik.touched.curso && formik.errors.curso ? true : false}
+                  {...formik.getFieldProps('curso')}/>
+                <FormFeedback>{formik.touched.curso && formik.errors.curso ? formik.errors.curso : null}</FormFeedback>
+              </FormGroup>
+            </Row>
+            <Row>
+              <Col>
+              <FormGroup>
+                <Label className="form-control-label" htmlFor="inicio">Ano Início *</Label>
+                <InputGroup className="input-group-alternative">
+                  <InputGroupAddon addonType="prepend">
+                    <InputGroupText>
+                      <i className="ni ni-calendar-grid-58" />
+                    </InputGroupText>
+                  </InputGroupAddon>
+                  <ReactDatetime
+                    inputProps={{
+                      id: "inicio",
+                      placeholder: "Data Início",
+                      // invalid: {formik:formik.touched.inicio && formik.errors.inicio ? true : false},
+                    }}
+                    locale="pt-br"
+                    timeFormat={false}
+                    // {...formik.getFieldProps('inicio')}
+                    />
+                    {/* <FormFeedback>{formik.touched.inicio && formik.errors.inicio ? formik.errors.inicio : null}</FormFeedback> */}
+                </InputGroup>
+                {/* <Input className="form-control-alternative" id="inicio" placeholder="Ano de Início" type="date"
+                  {...formik.getFieldProps('inicio')}/>*/}
+              </FormGroup>
+              </Col>
+              <Col>
+              <FormGroup>
+                <Label className="form-control-label" htmlFor="termino">Término *</Label>
+                <Input className="form-control-alternative" id="termino" placeholder="Ano Término" type="date"
+                  invalid={formik.touched.termino && formik.errors.termino ? true : false}
+                  {...formik.getFieldProps('termino')}/>
+                <FormFeedback>{formik.touched.termino && formik.errors.termino ? formik.errors.termino : null}</FormFeedback>
+              </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <FormGroup>
+                  <Label className="form-control-label" htmlFor="turno">Turno *</Label>
+                  <Input className="form-control-alternative" id="turno" type="select"
+                    invalid={formik.touched.turno && formik.errors.turno ? true : false}
+                    {...formik.getFieldProps('turno')}>
+                    <option value={null}>Turno</option>
+                    <option value="manha">Manhã</option>
+                    <option value="tarde">Tarde</option>
+                    <option value="noite">Noite</option>
+                    </Input>
+                  <FormFeedback>{formik.touched.turno && formik.errors.turno ? formik.errors.turno : null}</FormFeedback>
+                </FormGroup>
+              </Col>
+              <Col>
+                <FormGroup>
+                  <Label className="form-control-label" htmlFor="status">Status *</Label>
+                  <Input className="form-control-alternative" id="status" type="select"
+                    invalid={formik.touched.status && formik.errors.status ? true : false}
+                    {...formik.getFieldProps('status')}>
+                    <option value={null}>Status</option>
+                    <option value="concluido">Concluído</option>
+                    <option value="andamento">Cursando</option>
+                    <option value="trancado">Trancado</option>
+                    <option value="incompleto">Incompleto</option>
+                  </Input>
+                  <FormFeedback>{formik.touched.status && formik.errors.status ? formik.errors.status : null}</FormFeedback>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Button color="secondary" type="button" onClick={() => setModalOpen(!modalOpen)}>
+              Cancelar
+            </Button>
+            <Button color="primary" type="submit">
+              Salvar
+            </Button>
+          </Form>
+        </div>
+      </Modal>
+    </>
+  );
+}
