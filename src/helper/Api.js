6d0c8b622e5b94f6fdbd1bfc93759fd4299f5111
Breno Nogueira Araújo <breno.nog.araujo@hotmail.com>
2021-05-25 16:02:38 -0400

    commit login

--- src/helper/Api.js
+++ src/helper/Api.js
@@ -1,5 +1,5 @@
 
-//const BASE_URL = 'http://35.199.83.195:3333/';
-const BASE_URL = 'http://localhost:3333/';
+const BASE_URL = 'http://35.199.83.195:3333/';
+// const BASE_URL = 'http://localhost:3333/';
 
 export default(BASE_URL);
