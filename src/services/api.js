9e8353722ddccd2591096237195610e522161c8b
Theo <teo.pvh@gmail.com>
2021-05-26 11:38:02 -0400

    redux

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 const BASE_URL = process.env.REACT_APP_API_BASEURL;
 
-// const BASE_URL = 'http://35.199.83.195:3333/';
 // const BASE_URL = 'http://localhost:3333/';
 
 export const api_cadastro = `${BASE_URL}/cadastro`
