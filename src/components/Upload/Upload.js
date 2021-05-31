23f0c2ad59c41a7756a58deda362f9b73f7d53b9
Theo Victor <teo.pvh@gmail.com>
2021-05-31 00:05:37 -0400

    add campo upload de foto

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,12 +1,74 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import {Button} from 'reactstrap';
 
-export default function Upload(){
+import defaultImage from "assets/img/image_placeholder.jpg";
+import defaultAvatar from "assets/img/placeholder.jpg";
 
-
-  return(
-    <>
-
-    </>
+export default function Upload(props){
+  const [file, setFile] = React.useState(null);
+  const [imagePreviewUrl, setImagePreviewUrl] = React.useState(
+    props.avatar ? defaultAvatar : defaultImage
+  );
+  const fileInput = React.createRef();
+  const handleImageChange = (e) => {
+    e.preventDefault();
+    let reader = new FileReader();
+    let file = e.target.files[0];
+    reader.onloadend = () => {
+      setFile(file);
+      setImagePreviewUrl(reader.result);
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+  // const handleSubmit = e => {
+  // e.preventDefault();
+  // this.state.file is the file/image uploaded
+  // in this function you can save the image (this.state.file) on form submit
+  // you have to call it yourself
+  // };
+  const handleClick = () => {
+    fileInput.current.click();
+  };
+  const handleRemove = () => {
+    setFile(null);
+    setImagePreviewUrl(props.avatar ? defaultAvatar : defaultImage);
+    fileInput.current.value = null;
+  };
+  return (
+    <div className="fileinput text-center">
+      <input type="file" onChange={handleImageChange} ref={fileInput} />
+      <div
+        className={
+          "fileinput-new thumbnail img-raised" +
+          (props.avatar ? " img-circle" : "")
+        }
+      >
+        <img className="fileImg" src={imagePreviewUrl} alt="..." />
+      </div>
+      <div>
+        {file === null ? (
+          <Button className="btn-round" color="default" onClick={handleClick}>
+            {props.avatar ? "Add Foto" : "Select image"}
+          </Button>
+        ) : (
+          <span>
+            <Button className="btn-round" color="default" onClick={handleClick}>
+              Alterar
+            </Button>
+            {props.avatar ? <br /> : null}
+            <Button color="danger" className="btn-round" onClick={handleRemove}>
+              <i className="fa fa-times" /> Remover
+            </Button>
+          </span>
+        )}
+      </div>
+    </div>
   );
 }
+
+Upload.propTypes = {
+  avatar: PropTypes.bool,
+};
--- src/pages/Configuracao.js
+++ src/pages/Configuracao.js
@@ -1,14 +1,19 @@
 import React, {useEffect}from 'react';
-import {Container, Col, Row} from 'reactstrap';
+// import {Container, Col, Row} from 'reactstrap';
 import { useHistory } from "react-router-dom";
 import DashNavbar from 'components/Navbars/DashNavbar';
 import AccountSettings from 'components/Configuracao/AccountSettings';
 import isLoged from "helpers/isLoged";
+
+import 'assets/css/navbar.css'
+import 'assets/css/upload-img.css'
+
 export default function Configuracao(){
   const history = useHistory();
   const routeChange = () =>{ 
     history.push('/');
   }
+
   useEffect(() => {
     document.body.classList.add('settings');
     window.scrollTo(0, 0);
@@ -17,28 +22,13 @@ export default function Configuracao(){
       document.body.classList.remove('settings');
     };
   }, []);
+
   if(!isLoged()){routeChange()};
+
   return(
     <>
-      <DashNavbar type="transparent"/>
+      <DashNavbar/>
       <div className="wrapper">
-        {/* <div className="section-shaped my-0 skew-separator skew-mini">
-          <div className="page-header page-header-small header-filter">
-            <div className="page-header-image" style={{backgroundImage:'url("'+require("assets/img/theme/curved2.jpg")+'")',}}/>
-            <Container>
-              <div className="header-body text-center mb-7">
-                <Row className="justify-content-center">
-                  <Col className="px-5" lg="6" md="8" xl="5">
-                    <h1 className="text-white">Bem Vindo!</h1>
-                    <p className="text-lead text-white">
-                      Entre com sua conta, ou cria uma gratuitamente!!
-                    </p>
-                  </Col>
-                </Row>
-              </div>
-            </Container>
-          </div>
-        </div> */}
         <section className="section section-shaped section-lg">
           <div className="shape shape-style-1"
             style={{
